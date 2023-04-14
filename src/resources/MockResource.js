9a6b182e34c2c59e6b952b701c38ed5094101672
Felipe Pavanelli <felipepavanelli1@gmail.com>
2023-04-14 18:14:19 -0300

    feat: criando MockResource

---
+++ src/resources/MockResource.js
@@ -0,0 +1,41 @@
+const AdminJS = require('adminjs')
+
+const Mock = require('../models/Mock')
+
+module.exports = {
+    resource: Mock,
+    options: {
+        parent: {
+            icon: 'Json'
+        },
+        properties: {
+            mockName: {
+                position: 2,
+            },
+            description: {
+                position: 3,
+            },
+            response: {
+                position: 4,
+            },
+            statusCode: {
+                position: 5,
+            },
+            requestIdentifier: {
+                position: 6,
+                type: 'key-value'
+            },
+            api: {
+                position: 7,
+            },
+            createdAt: {
+                position: 8,
+                isVisible: { list: false, show: false, edit: false }
+            },
+            updatedAt: {
+                position: 9,
+                isVisible: { list: false, show: false, edit: false }
+            }
+        }
+    }
+}
