1be1857c94bdd26df5afbb54c2ec12ff0c5bdc78
Felipe Pavanelli <felipepavanelli1@gmail.com>
2023-04-14 23:32:57 -0300

    feat: removendo logo padrão da biblioteca

--- src/server.js
+++ src/server.js
@@ -17,7 +17,12 @@ const app = express()
 const adminJS = new AdminJS({
     databases: [],
     rootPath: "/admin",
-    resources: [ProjectResource, ApiResource]
+    resources: [ProjectResource, ApiResource],
+    branding: {
+        companyName: 'Mock APIs',
+        logo: false,
+        softwareBrothers: false,
+    }
 })
 
 const router = AdminJSExpress.buildRouter(adminJS)
